ec4e22385a6e15ec9a9294900226c01a42b554c4
DuongNT <DuongNT@splus-software.com.vn>
2020-07-29 17:15:05 +0700

    add ads function

--- src/WatchMovie.jsx
+++ src/WatchMovie.jsx
@@ -1,16 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import VideoPlayer from "./components/VideoJS";
 
-const sources = [
+const initSources = [
   {
     src: "http://techslides.com/demos/sample-videos/small.webm",
     type: "video/webm",
     label: "360P",
-  },
-  {
-    src: "http://media.w3.org/2010/05/video/movie_300.webm",
-    type: "video/webm",
-    label: "720P",
   },
   {
     src: "https://www.youtube.com/watch?v=xjS6SftYQaQ",
@@ -18,12 +13,49 @@ const sources = [
     label: "480P",
     selected: true,
   },
+  {
+    src: "http://media.w3.org/2010/05/video/movie_300.webm",
+    type: "video/webm",
+    label: "720P",
+  },
 ];
 
 function WatchMovie() {
+  const [sources, setSources] = useState(initSources);
+
+  const handleOceans = () => {
+    setSources([
+      {
+        src: "http://media.w3.org/2010/05/video/movie_300.webm",
+        type: "video/webm",
+        label: "720P",
+      },
+      {
+        src: "http://media.w3.org/2010/05/video/movie_300.webm",
+        type: "video/webm",
+        label: "360P",
+      },
+    ]);
+  };
+
+  const handleYoutube = () => {
+    setSources([
+      {
+        src: "https://www.youtube.com/watch?v=L0MK7qz13bU",
+        type: "video/youtube",
+      },
+    ]);
+  };
+
   return (
     <div>
       <VideoPlayer src={sources} />
+      <button className="oceans" onClick={handleOceans}>
+        Oceans
+      </button>
+      <button className="youtube" onClick={handleYoutube}>
+        YouTube
+      </button>
     </div>
   );
 }
--- src/components/VideoJS.jsx
+++ src/components/VideoJS.jsx
@@ -1,25 +1,44 @@
 import React, { useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
 import "video.js/dist/video-js.min.css";
+import "@silvermine/videojs-quality-selector/dist/css/quality-selector.css";
+import "videojs-contrib-ads/dist/videojs.ads.css";
+import "./custom.css";
 import videojs from "video.js";
-// import YoutubePlugin from "videojs-youtube";
 require("videojs-youtube/dist/Youtube");
+require("videojs-contrib-ads/dist/videojs.ads.min.js");
 require("@silvermine/videojs-quality-selector")(videojs);
 
-const usePlayer = ({ src, controls, autoplay }) => {
-  const options = {
-    // fill: true,
-    fluid: true,
-    preload: "auto",
-    html5: {
-      hls: {
-        enableLowInitialPlaylist: true,
-        smoothQualityChange: true,
-        overrideNative: true,
-      },
+const options = {
+  // fill: true,
+  // fluid: true,
+  preload: "auto",
+  html5: {
+    hls: {
+      enableLowInitialPlaylist: true,
+      smoothQualityChange: true,
+      overrideNative: true,
     },
-    playbackRates: [0.5, 1, 1.5, 2],
-  };
+  },
+  width: 640,
+  height: 480,
+  techOrder: ["youtube", "html5"],
+  controlBar: {
+    children: [
+      "playToggle",
+      "volumePanel",
+      "currentTimeDisplay",
+      "progressControl",
+      "DurationDisplay",
+      "playbackRateMenuButton",
+      "qualitySelector",
+      "fullscreenToggle",
+    ],
+  },
+  playbackRates: [0.5, 1, 1.5, 2],
+};
+
+const usePlayer = ({ src, controls, autoplay }) => {
   const videoRef = useRef(null);
   const [player, setPlayer] = useState(null);
 
@@ -34,11 +53,10 @@ const usePlayer = ({ src, controls, autoplay }) => {
     /**
      * Add handle event
      **/
-
-    vjsPlayer.on("seeking", function () {
-      console.log("seeking");
-    });
-
+    // vjsPlayer.on("seeking", function () {
+    //   console.log("seeking");
+    // });
+    handleAds(vjsPlayer);
     setPlayer(vjsPlayer);
 
     return () => vjsPlayer.dispose();
@@ -55,6 +73,38 @@ const usePlayer = ({ src, controls, autoplay }) => {
     }
   }, [src]);
 
+  function handleAds(player) {
+    player.ads(); // initialize videojs-contrib-ads
+    // request ads whenever there's new video content
+    player.on("contentchanged", function () {
+      // in a real plugin, you might fetch new ad inventory here
+      player.trigger("adsready");
+    });
+
+    player.on("readyforpreroll", function () {
+      player.ads.startLinearAdMode();
+      // play your linear ad content
+      // in this example, we use a static mp4
+      player.src({
+        src: "http://techslides.com/demos/sample-videos/small.webm",
+        type: "video/webm",
+      });
+
+      // send event when ad is playing to remove loading spinner
+      player.one("adplaying", function () {
+        player.trigger("ads-ad-started");
+      });
+
+      // resume content when all your linear ads have finished
+      player.one("adended", function () {
+        player.ads.endLinearAdMode();
+      });
+    });
+
+    // in a real plugin, you might fetch ad inventory here
+    player.trigger("adsready");
+  }
+
   return videoRef;
 };
 
@@ -63,12 +113,7 @@ const VideoPlayer = ({ src, controls, autoplay }) => {
 
   return (
     <div data-vjs-player>
-      <video
-        ref={playerRef}
-        className="video-js"
-        data-setup='{ "techOrder": ["youtube", "html5"] }'
-        poster="//vjs.zencdn.net/v/oceans.png"
-      />
+      <video ref={playerRef} className="video-js" />
     </div>
   );
 };
